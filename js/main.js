59afc9558c7557ab856587737ff96e391810f5f1
wiolettamehiadin <wioletta_k@live.co.uk>
2021-05-23 20:54:34 +0100

    Rozpoczecie pliku

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 10;
+let secondNumber = 20;
+
+function add (param1, param2) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+add (10,20);
+
+console.log(add (10,20));
